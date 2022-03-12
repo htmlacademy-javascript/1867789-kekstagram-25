1ddfc2599ff57296a0fb7571727331d04b006450
Keks <keks@htmlacademy.ru>
2022-03-12 13:28:39 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,13 @@
+import './util.js';
+import './load-image.js';
+import './image-editing.js';
+import './filter.js';
+import './form-data.js';
+import './picture.js';
+import './filter-picture.js';
+import './tags.js';
+import {getNewObjects} from './data.js';
+
+window.console.log(
+  getNewObjects()
+);
---
+++ 4/js/util.js
@@ -0,0 +1,23 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => (
+  elements[getRandomPositiveInteger(0, elements.length - 1)]
+);
+
+// Функция для проверки максимальной длины строки
+const MAX_SYMBOLS = 140;
+function controlComment (commentFieldText) {
+  if (commentFieldText.length <= MAX_SYMBOLS) {
+    return true;
+  }
+  return false;
+}
+controlComment ('i love cats');
+
+export {getRandomPositiveInteger, getRandomArrayElement};
