37c4f29de747209e3d291fd85f30663cfebb5ed4
Keks <keks@htmlacademy.ru>
2022-03-22 21:36:47 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './load-image.js';
+import './image-editing.js';
+import './filter.js';
+import './form-data.js';
+import './picture.js';
+import './filter-picture.js';
+import './tags.js';
