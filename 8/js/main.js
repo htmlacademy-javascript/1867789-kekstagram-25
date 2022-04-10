1ac780b364f9e6010813447e793902df5257dcae
Keks <keks@htmlacademy.ru>
2022-04-10 15:02:41 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './load-image.js';
+import './scale.js';
+import './filter.js';
+import './picture.js';
+import './filter-picture.js';
+import './form-validation.js';
+import './big-picture.js';
