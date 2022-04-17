119445538ceb35c6b157de0af2fe923e40f686c4
ViktoriiaShtyreva <vika1411akva0903@icloud.com>
2022-04-17 11:55:24 +0300

    Надо подкачаться

--- js/picture.js
+++ js/picture.js
@@ -1,28 +1,29 @@
-import {dataPhotos} from './data.js';
 import {showBigPhoto} from './big-picture.js';
 
 const picture = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const newPicture = dataPhotos;
+
 // Шаблон для фотографии пользователя
 const newPictureFragment = document.createDocumentFragment();
-// Генерация данных с url, likes, comment
-const createPicture = () => {
-  newPicture.forEach((photo) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = photo.url;
-    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
-    pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
-    pictureElement.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      showBigPhoto(photo);
-    });
-    newPictureFragment.appendChild(pictureElement);
+// Заполнение шаблона
+const createPicture = (photo) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  pictureElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPhoto(photo);
+  });
+  return pictureElement;
+};
+
+const createPictureFragment = (pictures) => {
+  pictures.forEach((photo)=> {
+    const photoElement = createPicture(photo);
+    newPictureFragment.appendChild(photoElement);
   });
   picture.appendChild(newPictureFragment);
 };
 
-
-// Создание картинки используя массив данных из data.js
-createPicture();
-
+export {createPictureFragment};
--- js/util.js
+++ js/util.js
@@ -22,4 +22,28 @@ controlComment ('i love cats');
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomPositiveInteger, getRandomArrayElement, isEscapeKey};
+// Показ сообщения об отправке с ошибкой на 5 секунд
+const showAlert = (message) => {
+  const ALERT_SHOW_TIME = 5000;
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+
+export {getRandomPositiveInteger, getRandomArrayElement, isEscapeKey, showAlert};
